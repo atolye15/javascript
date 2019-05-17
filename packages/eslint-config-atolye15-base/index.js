a78411d5a3c0837c0765859afb4456343b3c7319
Ali İhsan <ali@atolye15.com>
2019-05-17 11:47:27 +0300

    remove editorconfig - add prettier

--- packages/eslint-config-atolye15-base/index.js
+++ packages/eslint-config-atolye15-base/index.js
@@ -1,31 +1,23 @@
-
 module.exports = {
-  "parser": "@typescript-eslint/parser",
-  "extends": [
-    "airbnb",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:prettier/recommended",
-    "plugin:eslint-comments/recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "plugin:jest/recommended"
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:eslint-comments/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'plugin:jest/recommended',
   ],
-  "env": {
-    "browser": true,
-    "jest": true
+  env: {
+    browser: true,
+    jest: true,
   },
-  "plugins": [
-    "@typescript-eslint",
-    "jsx-a11y",
-    "import",
-    "prettier",
-    "jest",
-    "eslint-comments"
-  ],
-  "rules": {
-    "@typescript-eslint/indent": "off",
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "prettier/prettier": ["error"]
-  }
-}
+  plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'prettier', 'jest', 'eslint-comments'],
+  rules: {
+    '@typescript-eslint/indent': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    'prettier/prettier': ['error'],
+  },
+};

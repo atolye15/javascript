1d33602adf25c17af2efd06a3cda592e51581225
Eren Kurnaz <h.erenkurnaz@icloud.com>
2021-11-25 09:39:15 +0300

    Add new rules to typescript

--- packages/eslint-config-base-typescript/index.js
+++ packages/eslint-config-base-typescript/index.js
@@ -12,5 +12,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
 };

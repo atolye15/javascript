e004088e7b31bb6735c528cb47bafdde380cf03b
Eren Kurnaz <h.erenkurnaz@icloud.com>
2021-11-25 11:19:48 +0300

    Update rules

--- packages/eslint-config-base-typescript/index.js
+++ packages/eslint-config-base-typescript/index.js
@@ -10,10 +10,8 @@ module.exports = {
     '@typescript-eslint',
   ],
   rules: {
-    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-function-return-type': ['warn'],
     '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/interface-name-prefix': 'off',
-    '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': ['error'],
   },
 };
